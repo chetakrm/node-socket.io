08e85760ee34c857f13705ddbe6eab956502c516
crmadhyastha <crmadhyastha@gmail.com>
2018-05-11 03:00:25 -0400

    shree

--- server.js
+++ server.js
@@ -15,6 +15,7 @@ const io = socketIO(server);
 
 io.on('connection', (socket) => {
   console.log('Client connected');
+  socket.on('shreedakshina',(data)=>{io.emit('shreedakshina',data)});
   socket.on('disconnect', () => console.log('Client disconnected'));
 });
 
